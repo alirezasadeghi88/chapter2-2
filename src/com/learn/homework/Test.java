8a906dcc29024e6596e28d83cf333cafb6dbf100
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-01-07 02:45:40 -0800

    add Test

---
+++ src/com/learn/homework/Test.java
@@ -0,0 +1,11 @@
+package com.learn.homework;
+
+public class Test {
+    public static void main(String[] args) {
+        double i = 50.0;
+        double k = i + 50.0;
+        double j = k + 10;
+
+        System.out.println("j is " + j + " andk is " + k);
+    }
+}
