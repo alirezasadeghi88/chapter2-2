6632993aa662fb44f78b22e07fc53267e953cfc9
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-01-07 02:53:09 -0800

    add Test2

---
+++ src/com/learn/homework/Test2.java
@@ -0,0 +1,10 @@
+package com.learn.homework;
+
+public class Test2 {
+    public static void main(String[] args) {
+        int k = 20;
+        int i = k + 2;
+
+        System.out.println(i);
+    }
+}
